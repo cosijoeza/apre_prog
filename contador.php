923459d9f3090cc5940f8f4b35198dae598e70d0
Cosi Melchor Nolasco <cosi.melchor@gmail.com>
2017-12-06 21:52:57 -0600

    Add files via upload

---
+++ contador.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+        $i = contador();
+        echo $i;
+		function contador(){
+            $archivo = "contador.txt"; //el archivo que contiene en numero
+            $f = fopen($archivo, "r"); //abrimos el archivo en modo de lectura
+
+            $contador = fread($f, filesize($archivo)); //leemos el archivo
+            $contador = $contador + 1; //sumamos +1 al contador
+            fclose($f);
+
+            $f = fopen($archivo, "w+");
+            fwrite($f, $contador);
+            fclose($f);
+        return $contador;
+    }
+	?>
+</body>
+</html>
