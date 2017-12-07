7e661e697e4d2f4920bd3772800489c430a01429
Cosi Melchor Nolasco <cosi.melchor@gmail.com>
2017-12-06 21:58:32 -0600

    Add files via upload

--- contador.php
+++ contador.php
@@ -8,18 +8,19 @@
         $i = contador();
         echo $i;
 		function contador(){
-            $archivo = "contador.txt"; //el archivo que contiene en numero
-            $f = fopen($archivo, "r"); //abrimos el archivo en modo de lectura
+        	$archivo = "contador.txt"; //el archivo que contiene en numero
+        	$f = fopen($archivo, "r"); //abrimos el archivo en modo de lectura
 
             $contador = fread($f, filesize($archivo)); //leemos el archivo
             $contador = $contador + 1; //sumamos +1 al contador
             fclose($f);
 
-            $f = fopen($archivo, "w+");
-            fwrite($f, $contador);
+        	$f = fopen($archivo, "w+");
+            fwrite($f,$contador);
             fclose($f);
-        return $contador;
-    }
+        	return $contador;
+    	}
+
 	?>
 </body>
 </html>
